77f07fd7f5f0813b780bf1023d9511bb24e50c86
Chester How <chesterhow@gmail.com>
2023-01-16 18:06:10 +0800

    feat: display attraction rates for mice

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,29 @@
-import { ActionPanel, Detail, List, Action } from "@raycast/api";
+import { Action, ActionPanel, List } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
+import { AttractionRateList } from "./AttractionRateList";
 
 export default function Command() {
+  const { isLoading, data } = useFetch<string>(
+    "https://mhct.win/searchByItem.php?item_id=all&item_type=mouse&timefilter=all_time&min_hunts=100"
+  );
+
+  const parsedData: Mouse[] = typeof data === "string" ? JSON.parse(data) : [];
+
   return (
-    <List>
-      <List.Item
-        icon="list-icon.png"
-        title="Greeting"
-        actions={
-          <ActionPanel>
-            <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
-          </ActionPanel>
-        }
-      />
+    <List isLoading={isLoading} searchBarPlaceholder="Search for mice">
+      <List.EmptyView title="No matching mice found" description="Try a different search?" />
+
+      {parsedData.map((mouse, index) => (
+        <List.Item
+          key={index}
+          title={mouse.value}
+          actions={
+            <ActionPanel>
+              <Action.Push title="Show Attraction Rates" target={<AttractionRateList mouse={mouse} />} />
+            </ActionPanel>
+          }
+        />
+      ))}
     </List>
   );
 }
